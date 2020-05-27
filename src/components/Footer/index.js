2e4cff67b3437ebcf0d6ad7dad160347b544dfb5
unknown <aodong.zheng@seetatech.com>
2020-05-27 16:37:14 +0800

    完善 页脚

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,43 @@
-import React from "react"
-import { Header } from 'semantic-ui-react'
+import React from 'react'
+import { FooterWrap } from './style'
+import { Divider, Image, Label } from 'semantic-ui-react'
+import avatar from './dajiangyou.jpg'
 
 function Footer () {
   return (
-    <div style={{ background: 'black', paddingTop: '20vh' }}>
-      <Header as='h1' style={{ textAlign: 'center', color: 'white' }}>
-        The end.
-      </Header>
-    </div>
+    <FooterWrap>
+      <div className='row-top'>
+        <div className='col-left'>
+          <Image src={ require('./qrcode.png') } size='tiny'></Image>
+          <h1 className='title' style={{ marginTop: '10px' }}>Preview</h1>
+        </div>
+        <div className='col-center'>
+          <h1 className='title'>文档链接</h1>
+          <div className='item__wrap'>
+            <p>React 官方文档</p>
+            <p>Semantic UI</p>
+            <p>Node</p>
+          </div>
+        </div>
+        <div className='col-right'>
+          <h1 className='title'>相关链接</h1>
+          <div className='item__wrap'>
+            <p>GitHub</p>
+            <p>掘金</p>
+            <p>简书</p>
+          </div>
+        </div>
+      </div>
+      <Divider />
+      <div className='row-bottom'>
+        <span style={{ marginRight: '4px' }}> Design By </span>
+        <Label as='a' size='mini' image>
+          <img src={avatar} />
+          打酱油
+        </Label>
+        <p>copyright©2020</p>
+      </div>
+    </FooterWrap>
   )
 }
 
---
+++ src/components/Footer/style.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+export const FooterWrap = styled.footer`
+  color: white;
+  background: black;
+  min-height: 20vh;
+  padding: 40px 4% 20px 4%;
+
+  .row-top {
+    display: flex;
+
+    .col-left {
+      flex: 1;
+    }
+
+    .col-center {
+      flex: 1;
+    }
+
+    .col-right {
+      flex: 1;
+    }
+
+    .title {
+      font-size: 18px;
+    }
+
+    .item__wrap {
+      color: gray;
+      margin-top: 20px;
+      p {
+        margin-top: 4px;
+        &:hover {
+          color: teal;
+          cursor: pointer;
+        }
+      }
+    }
+
+  }
+
+  .row-bottom {
+    text-align: center;
+    font-size: 12px;
+  }
+
+`
