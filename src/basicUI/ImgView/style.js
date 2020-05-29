d138155e220e42ab3bcc5c2b690eee6df7e308c4
unknown <aodong.zheng@seetatech.com>
2020-05-29 17:49:19 +0800

    优化 图片组件

---
+++ src/basicUI/ImgView/style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const ImgWrap = styled.div`
+  position: relative;
+  display: inline-block;
+
+  :hover {
+    .dim__wrap {
+      display: block;
+    }
+  }
+
+  .dim__wrap {
+    display: none;
+    position: absolute;
+    width: 100%;
+    bottom: 0px;
+    background: rgba(128, 128, 128, 0.7);
+    padding: 4px;
+    text-align: right;
+  }
+
+
+`
