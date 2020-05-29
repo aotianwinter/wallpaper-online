790d50461d01d0ec7935b30d8122f79f4260a469
unknown <aodong.zheng@seetatech.com>
2020-05-29 17:48:57 +0800

    优化 图片预览

--- src/basicUI/ImgPreview/index.js
+++ src/basicUI/ImgPreview/index.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import { Dimmer, Image, Button } from 'semantic-ui-react'
-import { download } from './download'
 
 function ImgPreview (props) {
-  const { url, visible, tag } = props
-
-  const handleDownload = (url) => {
-    download(url, url)
-  }
+  const { url, utag } = props.previewImg
 
   return (
-    <Dimmer active={ visible } onClickOutside={props.handleClick} page>
-      <Image style={{ maxHeight: '90vh' }} src={url} title={ tag } alt={ tag } />
-      <Button onClick={ () => handleDownload(url) } style={{ marginTop: '20px' }} circular color='green' icon='download' />
+    <Dimmer active={ props.visible } onClickOutside={props.handleClick} page>
+      <Image style={{ maxHeight: '90vh' }} src={ url } title={ utag } alt={ utag } />
     </Dimmer>
   )
 }
