f000c36bd58b706d1f7ead2eb4e40ddd88abe75e
unknown <aodong.zheng@seetatech.com>
2020-06-04 17:45:56 +0800

    update PageAbout

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Header, Label } from 'semantic-ui-react'
+import { PageAboutWrap } from './style'
 import avatar from '../../assets/author.jpg'
 
 function PageAbout (props) {
   return (
-    <div style={{ textAlign: 'center', marginTop: '20vh' }}>
+    <PageAboutWrap>
       <Header as='h1'>Resources are for learning only, not for commercial use.</Header>
+      <Header as='h3'>If you like this web, please click star in my github! Thank you!😘</Header>
       <Label as='a' size='large' image>
-        <img src={avatar} alt='author avatar' />
-        打酱油
+        <img src={avatar} alt='author avatar' />打酱油
       </Label>
-    </div>
+    </PageAboutWrap>
   )
 }
 
---
+++ src/views/About/style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const PageAboutWrap = styled.div`
+  text-align: center;
+  padding-top: 20vh;
+`
